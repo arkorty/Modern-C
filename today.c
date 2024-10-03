be6830ea392a100b804c0c9de95a14eff5662c43
Arkaprabha Chakraborty <arkorty@gmail.com>
2024-10-04 05:10:31 +0530

    perf: alloc 21 bytes as it is the maximum needed

--- today.c
+++ today.c
@@ -5,11 +5,11 @@ int main(void) {
     struct tm *today = &(struct tm){
         .tm_year = 2024 - 1900,
         .tm_mon = 10 - 1,
-        .tm_mday = 4,
+        .tm_mday = 3,
     };
 
     mktime(today);
-    auto buffer = (char[42]){};
-    strftime(buffer, 42, "%A, %F", today);
+    auto buffer = (char[21]){};
+    strftime(buffer, 21, "%A, %F", today);
     puts(buffer);
 }
